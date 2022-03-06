641aaa7431eb3e132184b0b3418b79f93ae114ec
James Grugett <jahooma@gmail.com>
2022-03-06 15:34:24 -0800

    Sign in with google

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,13 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js"
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+
+export async function signIn() {
+  const { user, session, error } = await supabase.auth.signIn({
+    provider: "google",
+  })
+  return { user, error }
+}
