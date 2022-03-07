ce01f632294adb3f5d847d1bc55f370e87d7f667
idamayer <66329727+idamayer@users.noreply.github.com>
2022-03-06 17:30:27 -0800

    you matched screen

---
+++ pages/MatchScreen.tsx
@@ -0,0 +1,26 @@
+import type { NextPage } from "next"
+import Head from "next/head"
+import BasePage from '../components/base-page'
+import { CheckIcon, CalendarIcon, XIcon } from '@heroicons/react/outline'
+
+export default function  MatchScreen ()  {
+  return (
+    <BasePage>
+    <div className="flex p-16 flex-col text-center container content-center items-center justify-center">
+      <h2 className="text-2xl ">They want to see you too!</h2>
+      <div className="m-8 mt-16 bg-orange-200 self-center w-60  shadow"><div>Location</div><div>Date</div><div>Time</div></div>
+      <button className="bg-lime-600 p-4 text-white text-lg mt-16 rounded-full">
+        <CheckIcon className="h-8 inline-block" aria-hidden="true" />
+         Confirm
+      </button>
+      <button className="text-slate-600"> <CalendarIcon className="text-slate-600 inline-block h-4" aria-hidden="true" /> Add to Google calendar</button>
+      <button className="text-slate-600"> <CalendarIcon className="text-slate-600 inline-block h-4" aria-hidden="true" /> Remind me an hour before</button>
+        
+      <button className="bg-gray-200 p-4 mt-16 rounded-sm">
+        <XIcon className="h-4 inline-block" aria-hidden="true" />
+        I changed my mind
+      </button>
+    </div>
+    </BasePage>
+  )
+}
--- pages/NextDate.tsx
+++ pages/NextDate.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next"
 import Head from "next/head"
 import BasePage from '../components/base-page'
+import { CheckIcon } from '@heroicons/react/outline'
 
 export default function  NextDate ()  {
   return (
@@ -8,8 +9,10 @@ export default function  NextDate ()  {
     <div className="flex p-16 flex-col text-center container content-center items-center justify-center">
       <h3 className="text-lg">Next Date:</h3>
       <h1 className="text-4xl m-3">Name</h1>
-      <div className="m-8 self-center w-60 bg-white shadow"><div>Date</div><div>Time</div></div>
-      <button className="bg-lime-600 w-32 h-8 rounded-lg">check mark</button>
+      <div className="m-8 mt-16 self-center w-60 bg-white shadow"><div>Date</div><div>Time</div></div>
+      <button className="bg-lime-600 h-12 w-12 mt-16 rounded-full">
+        <CheckIcon className="text-white " aria-hidden="true" /></button>
+        
     </div>
     </BasePage>
   )
