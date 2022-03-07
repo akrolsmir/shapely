6aa30f71e3aa3192abef308190349ce6d95b01a3
Austin Chen <akrolsmir@gmail.com>
2022-03-06 16:38:08 -0800

    Add a signout button to homepage

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,4 +1,4 @@
-import { createClient, User } from "@supabase/supabase-js"
+import { createClient, User } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
@@ -7,13 +7,18 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
 export async function signIn() {
   const { user, session, error } = await supabase.auth.signIn({
-    provider: "google",
+    provider: 'google',
   })
   return { user, error }
 }
 
+export async function signOut() {
+  const { error } = await supabase.auth.signOut()
+  return { error }
+}
+
 export async function createUser(user: User) {
-  await supabase.from("users").insert({
+  await supabase.from('users').insert({
     id: user.id,
     name: user.user_metadata.name,
     createTime: new Date(user.created_at).getTime(),
@@ -22,14 +27,14 @@ export async function createUser(user: User) {
 }
 
 export async function getUser(userId: string) {
-  return supabase.from("users").select().eq("id", userId).single()
+  return supabase.from('users').select().eq('id', userId).single()
 }
 
 export function listenForUser(userId: string, setUser: (user: any) => void) {
   return supabase
     .from(`users:id=${userId}`)
-    .on("UPDATE", (payload) => {
-      console.log("user updated", payload.new)
+    .on('UPDATE', (payload) => {
+      console.log('user updated', payload.new)
       setUser(payload.new)
     })
     .subscribe()
