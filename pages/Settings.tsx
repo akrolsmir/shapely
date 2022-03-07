7f8791490d62a2a6c254829a61bcdde36c74f521
Austin Chen <akrolsmir@gmail.com>
2022-03-06 18:34:33 -0800

    Fix NextJS build

--- pages/Settings.tsx
+++ pages/Settings.tsx
@@ -1,13 +1,18 @@
 import BasePage from '../components/base-page'
 
 export default function Settings(props: {}) {
-  return <BasePage>
-
-    TODO: settings: age range preferences, gender pref, location, max distance, 
-
-    note, hinge has (we could model as "dealbreakers"): ethnicity, religion, height, has children, family plans, education level, politics, drinking, smoking, marajuana, drugs
-
-
-    <button> Take a break </button>
-  </BasePage>
+  return (
+    <BasePage>
+      <p>
+        TODO: settings: age range preferences, gender pref, location, max
+        distance
+      </p>
+      <p>
+        note, hinge has (we could model as dealbreakers): ethnicity, religion,
+        height, has children, family plans, education level, politics, drinking,
+        smoking, marajuana, drugs
+      </p>
+      <button> Take a break </button>
+    </BasePage>
+  )
 }
